f9a9b26ed4b91e3f68146ab7c67aad1ce39434cb
damaris.villca <villca.dam16@gmail.com>
2024-05-29 09:06:46 -0300

    last

--- server/src/pages/api/pokemon/[id].json.ts
+++ server/src/pages/api/pokemon/[id].json.ts
@@ -2,7 +2,7 @@ import type { APIRoute } from "astro";
 import { deletePokemon } from "../../../services/pokemon";
 
 export const DELETE: APIRoute = async (context) => {
-  try {
+  
     
     if (context.params.id == null) {
       throw new Error('es medio dificil que no haya id pero bueno');
@@ -16,14 +16,6 @@ export const DELETE: APIRoute = async (context) => {
         'Access-Control-Allow-Origin': '*',
       }
     });
-  } catch (error) {
-    // BORRAR, TIRA ERROR... dps veo q onda
-    return new Response(JSON.stringify({ error: error.toString() }), {
-      status: 500,
-      headers: {
-        'content-type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-      }
-    });
-  }
+  
+
 };
